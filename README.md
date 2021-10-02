bd1601a2e6d55e31518728bd9a03f72036bdb3a9
Juri <jasonluxie@gmail.com>
2021-10-02 17:14:36 -0500

    Added logic/luxon/scripting
    Scripting includes api which calculates city and renders current weather conditions for said city.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # 3-2-hw-weather-dashboard
+
+## Problem Logic
+1. Creating today's weather card 
+    * Empty city name and current weather info containers
+    * Set Name (Date) + Icon in city container
+    * Generate 4 List elements with the relevant information and append them to the current weather info list 
+1. Generating 5 cards on click
+    * Empty Container
+    * Generate 5 cards using a loop 
+    * DateTime.now().plus({ days: 1 })
+1. Saving Search History
+    * Event delegation on container for buttons, check if user is actually clicking on button
+    * Condational Length: If container.length >= 8, then remove last (oldest) search term
+    * Create New Button with value of search container
+    * Prepend to container
+    * Set container to local storage and get container to populate on page load 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,88 @@
+let searchCityName = $("#search-city_name");
+let searchCityButton = $("#search-city_btn");
+let searchHistory = $("#search-history");
+let weatherCurrent = $("#weather-current");
+let weatherCurrentCity = $("#weather-current_city");
+let weatherCurrentInfo = $("#weather-current_info");
+let weatherFuture = $("#weather-future");
+let modal = $(".modal");
+let latitude;
+let longitude;
+const currentDate = luxon.DateTime.now();
 
+//To add time to luxon date, use .plus(argument), by default argument is in milliseconds, but accepts object with specification
+
+//API for opencagedata via cityname
+//https://api.opencagedata.com/geocode/v1/json?q=PLACENAME&key=1103f44dccd54a9da1a21ae0d6e5939d
+// If response.total_results == 0 then text is you must enter a valid location
+
+//API for openweathermap via latitude and logitude
+// https://api.openweathermap.org/data/2.5/onecall?lat=50.98&lon=11.33&exclude=minutely,hourly,alerts&appid=bd3eeed040d34d406331ccfe15a926a1
+// For weather icons: "http://openweathermap.org/img/wn/{icon-code}@2x.png"
+
+searchCityButton.on("click", showCurrentWeather);
+
+function showCurrentWeather() {
+    if (searchCityName.val() == "") {
+        return alert("You have to input a city name");
+    }
+    getCoordinates(searchCityName.val());
+}
+
+function showFutureWeather() {}
+
+function getCoordinates(cityname) {
+    $.ajax({
+        url:
+            "https://api.opencagedata.com/geocode/v1/json?q=" +
+            cityname +
+            "&key=1103f44dccd54a9da1a21ae0d6e5939d",
+        method: "GET",
+    }).then(function (response) {
+        // Modal validation for text name
+        // if (response.total_results <= 0) {
+        //     modal.addClass('is-active')
+        // }
+        // console.log(response.total_results)
+        latitude = response.results[0].geometry.lat;
+        longitude = response.results[0].geometry.lng;
+        cityData(latitude, longitude);
+    });
+}
+
+function cityData(lat, lon) {
+    $.ajax({
+        url:
+            "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+            lat +
+            "&lon=" +
+            lon +
+            "&units=imperial&exclude=minutely,hourly,alerts&appid=bd3eeed040d34d406331ccfe15a926a1",
+        method: "GET",
+    }).then(function (response) {
+        console.log(response);
+        // console.log(response.current.weather[0].icon)
+        let cityName = searchCityName.val();
+        searchCityName.val("")
+        weatherCurrentCity.html(
+            cityName +
+                "(" +
+                currentDate.toLocaleString() +
+                ")" +
+                "<img src=http://openweathermap.org/img/wn/" +
+                response.current.weather[0].icon +
+                "@2x.png>"
+        );
+        weatherCurrentInfo.html(
+            "<li>Temp: " + response.current.temp + " F</li>" +
+            "<li>Wind: " + response.current.wind_speed +" MPH</li>" +
+            "<li>Humidity: " + response.current.humidity + " %</li>" +
+            "<li id=\"uv-index\">UV Index: " + response.current.uvi + "</li>"
+        );
+    });
+}
+
+function saveSearches() {}
+
+// getCoordinates("Houston");
+// showCurrentWeather();
